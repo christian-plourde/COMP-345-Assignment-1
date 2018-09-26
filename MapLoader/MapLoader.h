155d9070cf4e2fc29776d2a5a0ac14d074864cf9
Christian Plourde <christian.bertin.plourde@gmail.com>
2018-09-25 21:56:36 -0400

    Started map loader class

---
+++ MapLoader/MapLoader.h
@@ -0,0 +1,16 @@
+#include "../Graph/Graph.h"
+#include <string>
+
+#ifndef MAP_LOADER_H
+#define MAP_LOADER_H
+//this header file contains the definition for the map loader class, which provides methods for loading map files
+//into the game and outputs a graph that contains the data in the map file
+
+class MapLoader
+{
+  private:
+    std::string filePath; //the path to the file containing the map
+
+};
+
+#endif
